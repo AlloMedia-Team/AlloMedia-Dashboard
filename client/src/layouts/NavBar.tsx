b3deadac8f14a86f037b92a25ee115e7daba7dfd
Ismail-Lafhiel <odashimidorida@gmail.com>
2024-10-29 10:12:45 +0100

    AD-31 created create restaurant modal

--- client/src/layouts/NavBar.tsx
+++ client/src/layouts/NavBar.tsx
@@ -16,24 +16,19 @@ const NavBar = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { user, token } = useSelector((state: RootState) => state.auth);
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [becomeManager, setBecomeManager] = useState(false);
+  const [createRestaurantModal, setCreateRestaurantModal] = useState(false);
 
   const toggleCart = () => {
     setIsCartOpen((prev) => !prev);
   };
 
-  const handleManager = () => {
-    setBecomeManager(true);
-    console.log(becomeManager);
+  const toggleCreateRestaurantModal = () => {
+    setCreateRestaurantModal(!createRestaurantModal);
   };
 
-  // useEffect(() => {
-  //   if (becomeManager) {
-  //     setTimeout(() => {
-  //       setBecomeManager(false);
-  //     }, 3000);
-  //   }
-  // }, [becomeManager]);
+  useEffect(() => {
+    console.log("Modal state changed:", createRestaurantModal);
+  }, [createRestaurantModal]);
 
   const handleLogout = async () => {
     await dispatch(logout());
@@ -53,23 +48,19 @@ const NavBar = () => {
               <div className="hidden sm:ml-6 sm:flex sm:items-center sm:space-x-4">
                 {user && token ? (
                   <>
-                    <Link to="">
-                      <Button
-                        onClick={handleManager}
-                        className="text-red-600 hover:text-white border border-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-md text-sm  text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-                        variant="ghost"
-                      >
-                        Become Manager
-                      </Button>
-                    </Link>
-                    <Link to="">
-                      <Button
-                        className="text-white hover:text-white border border-gray-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-sm text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
-                        variant="ghost"
-                      >
-                        Become Delivery man
-                      </Button>
-                    </Link>
+                    <Button
+                      onClick={toggleCreateRestaurantModal}
+                      className="text-red-600 hover:text-white border border-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-md text-sm  text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                      variant="ghost"
+                    >
+                      Become Manager
+                    </Button>
+                    <Button
+                      className="text-white hover:text-white border border-gray-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-sm text-center dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+                      variant="ghost"
+                    >
+                      Become Delivery man
+                    </Button>
                     <DropdownMenu>
                       <DropdownMenuTrigger asChild>
                         <User className="mr-2 h-6 w-6 text-white" />
@@ -289,6 +280,148 @@ const NavBar = () => {
           </div>
         </div>
       </nav>
+      {/* create restaurant modal */}
+      {createRestaurantModal && (
+        <div
+          onClick={(e) => {
+            if (e.target === e.currentTarget) toggleCreateRestaurantModal();
+          }}
+          className="flex flex-row overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full"
+        >
+          <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
+            <div className="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+              <div className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  Add Product
+                </h3>
+                <button
+                  type="button"
+                  onClick={toggleCreateRestaurantModal}
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-toggle="defaultModal"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              <form action="#">
+                <div className="grid gap-4 mb-4 sm:grid-cols-2">
+                  <div>
+                    <label
+                      htmlFor="name"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                      placeholder="Type product name"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="brand"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Brand
+                    </label>
+                    <input
+                      type="text"
+                      name="brand"
+                      id="brand"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                      placeholder="Product brand"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="price"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Price
+                    </label>
+                    <input
+                      type="number"
+                      name="price"
+                      id="price"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                      placeholder="$2999"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="category"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Category
+                    </label>
+                    <select
+                      id="category"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                    >
+                      <option selected="">Select category</option>
+                      <option value="TV">TV/Monitors</option>
+                      <option value="PC">PC</option>
+                      <option value="GA">Gaming/Console</option>
+                      <option value="PH">Phones</option>
+                    </select>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="description"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Description
+                    </label>
+                    <textarea
+                      id="description"
+                      rows="4"
+                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
+                      placeholder="Write product description here"
+                    ></textarea>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="text-white inline-flex items-center bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                >
+                  <svg
+                    className="mr-1 -ml-1 w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  Add new product
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
